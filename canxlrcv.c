85dc73ce01af8a22b6a06cf969587b5fa83b64b4
Oliver Hartkopp <socketcan@hartkopp.net>
2024-01-05 22:13:18 +0100

    canxlrcv: add filter features from candump tool
    Signed-off-by: Oliver Hartkopp <socketcan@hartkopp.net>

--- canxlrcv.c
+++ canxlrcv.c
@@ -33,6 +33,16 @@ void print_usage(char *prg)
 	fprintf(stderr, "         -V <vcid>:<vcid_mask> (VCID filter)\n");
 	fprintf(stderr, "         -P (check data pattern)\n");
 	fprintf(stderr, "\n");
+	fprintf(stderr, "Optional filter sets can be specified on the commandline\n");
+	fprintf(stderr, "in the form: <CAN interface>[,filter]*\n");
+	fprintf(stderr, "\nFilters:\n");
+	fprintf(stderr, "  Comma separated filters can be specified for each given CAN interface:\n");
+	fprintf(stderr, "    <can_id>:<can_mask>\n         (matches when <received_can_id> & mask == can_id & mask)\n");
+	fprintf(stderr, "    <can_id>~<can_mask>\n         (matches when <received_can_id> & mask != can_id & mask)\n");
+	fprintf(stderr, "    #<error_mask>\n         (set error frame filter, see include/linux/can/error.h)\n");
+	fprintf(stderr, "    [j|J]\n         (join the given CAN filters - logical AND semantic)\n");
+	fprintf(stderr, "\nCAN IDs, masks and data content are given and expected in hexadecimal values.\n");
+	fprintf(stderr, "Without any given filter all data frames are received ('0:0' default filter).\n");
 	fprintf(stderr, "Use interface name '%s' to receive from all CAN interfaces.\n", ANYDEV);
 }
 
@@ -43,12 +53,16 @@ int main(int argc, char **argv)
 	struct can_raw_vcid_options vcid_opts = {};
 	struct sockaddr_can addr;
 	struct ifreq ifr;
-	int ifindex = 0;
 	int max_devname_len = 0; /* to prevent frazzled device name output */
 	int nbytes, ret, i;
 	int sockopt = 1;
 	int vcid = 0;
 	int check_pattern = 0;
+	int numfilter;
+	int join_filter;
+	char *ptr, *nptr;
+	can_err_mask_t err_mask;
+	struct can_filter *rfilter;
 	struct timeval tv;
 	union {
 		struct can_frame cc;
@@ -87,8 +101,16 @@ int main(int argc, char **argv)
 		exit(0);
 	}
 
-	if (strlen(argv[optind]) >= IFNAMSIZ) {
-		printf("Name of CAN device '%s' is too long!\n\n", argv[optind]);
+	ptr = argv[optind];
+	nptr = strchr(ptr, ',');
+
+	if (nptr)
+		nbytes = nptr - ptr; /* interface name is up the first ',' */
+	else
+		nbytes = strlen(ptr); /* no ',' found => no filter definitions */
+
+	if (nbytes >= IFNAMSIZ) {
+		fprintf(stderr, "name of CAN device '%s' is too long!\n", ptr);
 		return 1;
 	}
 
@@ -98,21 +120,90 @@ int main(int argc, char **argv)
 		return 1;
 	}
 
+	memset(&ifr.ifr_name, 0, sizeof(ifr.ifr_name));
+	strncpy(ifr.ifr_name, ptr, nbytes);
+
+	if (strcmp(ANYDEV, ifr.ifr_name) != 0) {
+		if (ioctl(s, SIOCGIFINDEX, &ifr) < 0) {
+			perror("SIOCGIFINDEX");
+			exit(1);
+		}
+		addr.can_ifindex = ifr.ifr_ifindex;
+	} else
+		addr.can_ifindex = 0; /* any can interface */
+
+	addr.can_family = AF_CAN;
+
+	if (nptr) {
+		/* found a ',' after the interface name => check for filters */
+
+		/* determine number of filters to alloc the filter space */
+		numfilter = 0;
+		ptr = nptr;
+		while (ptr) {
+			numfilter++;
+			ptr++; /* hop behind the ',' */
+			ptr = strchr(ptr, ','); /* exit condition */
+		}
+
+		rfilter = malloc(sizeof(struct can_filter) * numfilter);
+		if (!rfilter) {
+			fprintf(stderr, "Failed to create filter space!\n");
+			return 1;
+		}
+
+		numfilter = 0;
+		err_mask = 0;
+		join_filter = 0;
+
+		while (nptr) {
+
+			ptr = nptr + 1; /* hop behind the ',' */
+			nptr = strchr(ptr, ','); /* update exit condition */
+
+			if (sscanf(ptr, "%x:%x",
+				   &rfilter[numfilter].can_id,
+				   &rfilter[numfilter].can_mask) == 2) {
+				rfilter[numfilter].can_mask &= ~CAN_ERR_FLAG;
+				numfilter++;
+			} else if (sscanf(ptr, "%x~%x",
+					  &rfilter[numfilter].can_id,
+					  &rfilter[numfilter].can_mask) == 2) {
+				rfilter[numfilter].can_id |= CAN_INV_FILTER;
+				rfilter[numfilter].can_mask &= ~CAN_ERR_FLAG;
+				numfilter++;
+			} else if (*ptr == 'j' || *ptr == 'J') {
+				join_filter = 1;
+			} else if (sscanf(ptr, "#%x", &err_mask) != 1) {
+				fprintf(stderr, "Error in filter option parsing: '%s'\n", ptr);
+				return 1;
+			}
+		}
+
+		if (err_mask)
+			setsockopt(s, SOL_CAN_RAW, CAN_RAW_ERR_FILTER,
+				   &err_mask, sizeof(err_mask));
+
+		if (join_filter && setsockopt(s, SOL_CAN_RAW, CAN_RAW_JOIN_FILTERS,
+					      &join_filter, sizeof(join_filter)) < 0) {
+			perror("setsockopt CAN_RAW_JOIN_FILTERS not supported by your Linux Kernel");
+			return 1;
+		}
+
+		if (numfilter)
+			setsockopt(s, SOL_CAN_RAW, CAN_RAW_FILTER,
+				   rfilter, numfilter * sizeof(struct can_filter));
+
+		free(rfilter);
+
+	} /* if (nptr) */
+
 	ret = setsockopt(s, SOL_CAN_RAW, CAN_RAW_XL_FRAMES,
 			 &sockopt, sizeof(sockopt));
 	if (ret < 0) {
 		perror("sockopt CAN_RAW_XL_FRAMES");
 		return 1;
 	}
-
-	if (strcmp(argv[optind], ANYDEV) != 0) {
-		strcpy(ifr.ifr_name, argv[optind]);
-		ioctl(s, SIOCGIFINDEX, &ifr);
-		ifindex = ifr.ifr_ifindex;
-	}
-
-	addr.can_family = AF_CAN;
-	addr.can_ifindex = ifindex;
 
 	if (vcid) {
 		vcid_opts.flags = CAN_RAW_XL_VCID_RX_FILTER;
